6dcda2305c096ebe9901ea596c59e6128fd9dbd7
aloeleaf <szabolcs.sandor@gmail.com>
2025-06-09 17:08:18 +0200

    docker php image javítva

--- php/helper.php
+++ php/helper.php
@@ -11,6 +11,18 @@ function get_server_info() {
     return $_SERVER['SERVER_SOFTWARE'] . ' running on ' . $_SERVER['SERVER_NAME'];
 }
 
+function connect_to_mariadb($host, $user, $password, $dbname) {
+    $conn = new mysqli($host, $user, $password, $dbname);
+    if ($conn->connect_error) {
+        return "Database connaecton: failed!" . $conn->connect_error;
+    } else {
+        $conn->close();
+        return "Database connection: OK!";
+    }
+}
+
+
+
 // Example usage
 // echo custom_function();
 // echo get_current_time();
--- web/index.php
+++ web/index.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>BKT Tárgyaló App</title>
+</head>
+<body>
+<h1>Welcome to the BKT Tárgyaló App</h1>
+<p><a href="phpinfo.php">Rendszer információ</a></p>
+
 <?php
 require '/var/www/php/helper.php';
 echo "Hello from web root!<br>";
@@ -5,3 +15,8 @@ echo custom_function();
 echo "<br>Current time: " . get_current_time() . "<br>";
 echo "Server info: " . get_server_info() . "<br>";
 echo "This is the web root index.php file.<br>";
+echo connect_to_mariadb('db', 'dbappuser', 'p1ssw2rd', 'bktAppdb');
+?>
+
+</body>
+</html>
